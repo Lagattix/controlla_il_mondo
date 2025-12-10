c0b68c0f1acae37308c4df3ae0a92e5ad4c8b8a5
Lagattix <giulagattu@gmail.com>
2025-12-10 10:56:33 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
+// Metti qui la tua config Firebase
 const firebaseConfig = {
-  apiKey: "…",
-  authDomain: "…",
-  projectId: "…",
-  storageBucket: "…",
-  messagingSenderId: "…",
-  appId: "…"
+  apiKey: "AIzaSyAj7tnUKJe0z9s5s9_IP0jBC_C01gJNATw",
+  authDomain: "controlla-il-mondo.firebaseapp.com",
+  projectId: "controlla-il-mondo",
+  storageBucket: "controlla-il-mondo.firebasestorage.app",
+  messagingSenderId: "800513797892",
+  appId: "1:800513797892:web:65681195d74b68c6f0ea51"
 };
 
 export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
